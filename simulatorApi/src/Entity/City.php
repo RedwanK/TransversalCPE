0f08c6f3d4b3d2641475944e355e89098cca6cae
Redwan <redwan@integral-service.fr>
2020-12-08 17:11:54 +0100

    initialize database structure and add api bundles

---
+++ simulatorApi/src/Entity/City.php
@@ -0,0 +1,83 @@
+<?php
+namespace App\Entity;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\Timestampable;
+use Symfony\Component\Validator\Constraints as Assert;
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="city")
+ */
+class City {
+
+    use Timestampable;
+
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", nullable=false)
+     * @Assert\NotBlank()
+     *
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", nullable=false)
+     * @Assert\NotBlank()
+     *
+     */
+    private $country;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     *
+     * @return City
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * @param mixed $country
+     *
+     * @return City
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+}
