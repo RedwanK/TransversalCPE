49ec6cb827f6303ebd5b0fe3d9b9af495cb06b35
lucianoBrd <lucien.burdet@gmail.com>
2020-12-09 12:58:10 +0100

    simulation with struct incident

---
+++ microbit/microbit-simulation/main.cpp
@@ -0,0 +1,44 @@
+#include "MicroBit.h"
+
+extern "C" {
+    #include "incident.h"
+};
+
+MicroBit uBit;
+MicroBitSerial serial(USBTX, USBRX);
+
+int main()
+{
+    // Initialise the micro:bit runtime.
+    uBit.init();
+    
+    serial.baud(115200);
+    
+    //TEst
+    float   latitude = 10.2f,
+            longitude = 15.5f,
+            intensity = 9.9f;
+    
+    incident *icd = create_object_incident(longitude, latitude, intensity);
+    char str[20];
+    sprintf(str, "%.2f", icd->longitude);
+    uBit.display.scroll(str);
+    delete_incident(icd);
+    icd = NULL;
+    
+    while(1){
+        int c = serial.read(ASYNC);
+        if ((char)c!=MICROBIT_NO_DATA){
+            if ((char)c != '#') {
+                ManagedString cms((char)c);
+                uBit.display.scroll(cms);
+            } else {
+            }
+            
+        }
+        //uBit.sleep(100);
+    }
+    
+    release_fiber();
+}
+
