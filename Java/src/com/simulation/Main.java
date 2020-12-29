1914b92b7acba9f0c0e2a297c08962dca75af6bd
MarouaneA <marouane.azzouz@etu.univ-lyon1.fr>
2020-12-29 05:32:05 +0100

    Ãtests with requests + start of directory overhaul (fixup this commit)

--- Java/src/com/simulation/Main.java
+++ Java/src/com/simulation/Main.java
@@ -1,8 +1,12 @@
 package com.simulation;
 
+import api.Api;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("chlib");
+        System.out.println("Main - starting...");
+        Api.test();
+        System.out.println("Main - ending...");
     }
 }
