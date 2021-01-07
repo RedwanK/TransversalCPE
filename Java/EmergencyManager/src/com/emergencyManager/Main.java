66203f262feeec7dd49b58c12c160f99e3bc23fb
Emilio-mld <em69002@gmail.com>
2021-01-07 15:59:59 +0100

    Minor changes on IncidentHandler and starting TeamGenerator

--- Java/EmergencyManager/src/com/emergencyManager/Main.java
+++ Java/EmergencyManager/src/com/emergencyManager/Main.java
@@ -1,5 +1,7 @@
 package com.emergencyManager;
 import java.util.ArrayList;
+import entities.Incident;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,6 +9,7 @@ public class Main {
         handler.incidentListener("start");
         ArrayList incidents = handler.latestIncidents();
         System.out.println("liste incidents : "+incidents);
+        System.out.println("incident 1 : "+((Incident)incidents.get(0)).getIntensity());
         handler.incidentListener("stop");
     }
 }
--- Java/EmergencyManager/src/com/emergencyManager/TeamGenerator.java
+++ Java/EmergencyManager/src/com/emergencyManager/TeamGenerator.java
@@ -1,4 +1,5 @@
 package com.emergencyManager;
 
 public class TeamGenerator {
+
 }
