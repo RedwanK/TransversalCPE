79980c612206b2baa9c4d9d12e99f6e0aed8d220
RedwanK <redwankara@gmail.com>
2021-01-13 18:39:49 +0100

    fix and finish simulator-bridge

--- bridge/simulator-bridge.py
+++ bridge/simulator-bridge.py
@@ -11,102 +11,75 @@ import json
 from multiprocessing import Process, Value, Lock, Array, Manager
 import requests
 
-LAST_VALUE      = b"TL" #derniere valeur remontée par le microbit (initialisée a TL arbitrairement) elle prend des valeurs json type : {t: 28, l: 0, o: "TL"}
-LAST_CLIENT_ADDRESS = "" #adresse du dernier client android avec qui le serveur a communiqué
-LAST_VALUE_RECEIVED = "" # dernière valeur recue
-
 
 # send serial message
-SERIALPORT = "/dev/tty" # port serie du microbit connecté via usb (ttyACMX pour linux, COMXX pour windows)
+SERIALPORT = "/dev/ttyACM1" # port serie du microbit connecté via usb (ttyACMX pour linux, COMXX pour windows)
 BAUDRATE = 115200 # baudrate par defaut
 ser = serial.Serial() # recuperation de la connexion serie
 
+ENV = "http://simulator-api.local"
+
 #methode d'initialisation de la connexion UART
 def initUART():
-        # ser = serial.Serial(SERIALPORT, BAUDRATE)
-        ser.port=SERIALPORT
-        ser.baudrate=BAUDRATE
-        ser.bytesize = serial.EIGHTBITS #number of bits per bytes
-        ser.parity = serial.PARITY_NONE #set parity check: no parity
-        ser.stopbits = serial.STOPBITS_ONE #number of stop bits
-        ser.timeout = None          #block read
+    ser.port=SERIALPORT
+    ser.baudrate=BAUDRATE
+    ser.bytesize = serial.EIGHTBITS
+    ser.parity = serial.PARITY_NONE
+    ser.stopbits = serial.STOPBITS_ONE
+    ser.timeout = None
 
-        # ser.timeout = 0             #non-block read
-        # ser.timeout = 2              #timeout block read
-        ser.xonxoff = False     #disable software flow control
-        ser.rtscts = False     #disable hardware (RTS/CTS) flow control
-        ser.dsrdtr = False       #disable hardware (DSR/DTR) flow control
-        #ser.writeTimeout = 0     #timeout for write
-        print('Starting Up Serial Monitor')
-        try:
-                ser.open()
-        except serial.SerialException:
-                print("Serial {} port not available".format(SERIALPORT))
-                exit()
+    ser.xonxoff = False
+    ser.rtscts = False
+    ser.dsrdtr = False
+    print('Starting Up Serial Monitor')
+    try:
+            ser.open()
+    except serial.SerialException:
+            print("Serial {} port not available".format(SERIALPORT))
+            exit()
 
-def on_publish(client,userdata,result):
-    print("data published \n")
-    pass
-
-# methode d'envoi d'un message par UART (a destination du microbit)
 def sendUARTMessage(msg):
-    ser.write(msg) # envoi du message
-    display = str(msg, 'UTF-8')
-    print("Message <" + display + "> sent to micro-controller." ) #retour visuel dans la console du serveur
+    ser.write(msg.encode())
 
 def retrieveApi(apiData):
-        print('retrieveApi')
-        while True:
-                response = requests.get("http://simulator-api.local/api/incidents/list")
-                if response != "":
-                        print(response.json())
-                        #apiData.append(response.json())
-                if exit_event.wait(timeout=5):
-                        break
+    print('retrieveApi')
+    while True:
+            response = requests.get(ENV + "/api/incidents/unresolved/list")
+            if response != "":
+                    list = response.json()
+                    for incident in list:
+                        if incident['code_incident'] not in apiData:
+                            apiData.append(incident['code_incident'])
+                    time.sleep(5)
 
-        print('END retrieveApi')
-        
-def signal_handler(signum, frame):
-        print('signal_handler')
-        exit_event.set()
+    print('END retrieveApi')
+
 
 def handle_uart(apiData):
-        print('handle_uart')
-        while ser.isOpen() :
-                print('one more round')
-                if exit_event.wait(timeout=3):
-                        print('exit handle_uart')
-                        break
-                if (ser.inWaiting() > 0): # if incoming bytes are waiting
-                        data_b = ser.readline() # recuperation des données en provenance de l'UART (microbit)
-                        data_str = str(data_b, 'UTF-8') #conversion des bits en string
-                        if data_str == "retry\n": #si une erreur est survenue = le microbit renvoie "retry\n"
-                                #sendUARTMessage(str.encode(LAST_VALUE_RECEIVED)) # alors on renvoie le dernier message envoyé pour relancer l'operation
-                                print("retry <" + LAST_VALUE_RECEIVED + ">") # retour visuel
-                        else:
-                                LAST_VALUE = data_b # sinon, tout s'est bien passé et on a reçu des données depuis le microbit
-                                print(data_str)
-        print('END handle_uart')
-        
+    print('handle_uart')
+    while ser.isOpen() :
+            for incident in apiData:
+                print(incident)
+                sendUARTMessage(incident)
+                time.sleep(5)
+                apiData.remove(incident)
+            time.sleep(1)
+    print('END handle_uart')
+
 # Main program logic follows:
 if __name__ == '__main__':
-        initUART() # initialisation uart
-        print ('Press Ctrl-C to quit.')
+    initUART() # initialisation uart
+    print ('Press Ctrl-C to quit.')
+    with Manager() as manager:
+        apiData = manager.list()
+        api = Process(target=retrieveApi, args= (apiData,))
+        api.start()
 
-        exit_event = Event()
-        signal.signal(signal.SIGINT, signal_handler)
-        
+        uart = Process(target=handle_uart, args= (apiData,))
+        uart.start()
 
-        with Manager() as manager:
-                apiData = manager.list()
-                apiThread = Thread(target=retrieveApi, args=(apiData,))  # Passing the list
-                apiThread.daemon = False
-                uartThread = Thread(target=handle_uart, args=(apiData,))
-                uartThread.daemon = False
-                apiThread.start()
-                uartThread.start()
-                apiThread.join()
-                uartThread.join()
-        
-        ser.close()
-        exit()
+        api.join()
+        uart.join()
+
+    ser.close()
+    exit()
