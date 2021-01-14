d71774ad20a4cfc0c95d25d53159fba596506738
RedwanK <redwankara@gmail.com>
2021-01-14 04:53:58 +0100

    update  webview parameters

--- webview-emergency/src/app/maps/data/map-page-options.ts
+++ webview-emergency/src/app/maps/data/map-page-options.ts
@@ -6,8 +6,8 @@ import { PageOptions } from '../interfaces/page-options';
 
 export const MapPageOptions: PageOptions =
 {
-  env: 'http://localhost/api',
+  env: 'http://emergency-api.local/api',
   incident: '/incidents/unresolved/list',
-  intervention: '/interventions/unresolved/list',
+  intervention: '/intervention/unresolved/list',
   sensor: '/sensors/list'
 };
